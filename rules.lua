10d75f400872dc558f320d02cedeae95bf2077e5
renqwee <xrayan.23@gmail.com>
2026-06-22 10:04:09 +0300

    add window opacity rules and sidebar transparency

---
+++ rules.lua
@@ -0,0 +1,13 @@
+-- This file will not be overwritten across dots-hyprland updates.
+-- The file name is for the sake of organization and does not matter
+-- See the corresponding files in ~/.config/hypr/hyprland for examples
+
+hl.window_rule({match = {class = "code"},                 opacity = "0.85 0.85"})
+hl.window_rule({match = {class = "kitty"},                opacity = "0.65 0.65"})
+hl.window_rule({match = {class = "Spotify"},              opacity = "0.55 0.55"})
+hl.window_rule({match = {class = "brave-browser"},        opacity = "0.85 0.85"})
+hl.window_rule({match = {class = "org.telegram.desktop"}, opacity = "0.75 0.75"})
+hl.window_rule({match = {class = "discord"},              opacity = "0.80 0.80"})
+hl.window_rule({match = {class = "org.kde.dolphin"},      opacity = "0.80 0.80"})
+hl.window_rule({match = {class = "steam"},                opacity = "0.70 0.70"})
+hl.window_rule({match = {class = "obsidian"},             opacity = "0.90 0.90"})
